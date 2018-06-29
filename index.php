0862f9e3f7f97c9933be39eb252cc6345a37d160
Steve Gehrman <sgehrman@gmail.com>
2018-06-29 01:41:46 -0700

    added meta data

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
+<meta name='viewport' content='width=device-width, initial-scale=1' />
 <meta charset="<?php bloginfo('charset'); ?>">
 <link href="<?php bloginfo('stylesheet_url'); ?>" rel="stylesheet">
 <?php wp_head(); ?>
